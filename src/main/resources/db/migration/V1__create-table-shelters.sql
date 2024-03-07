115ef8b282f0e9ee3577a35dc4be83d238d23e19
Crystian Lefundes <crystian.lf@gmail.com>
2024-03-07 17:04:51 -0300

    feat: Add migrations file version 1 - create table shelters

---
+++ src/main/resources/db/migration/V1__create-table-shelters.sql
@@ -0,0 +1,12 @@
+CREATE TABLE shelters (
+    id UUID NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    phone VARCHAR(14) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    address VARCHAR(100) NOT NULL,
+    complement VARCHAR(50),
+    number NUMERIC(4) NOT NULL,
+    state VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    zip_code VARCHAR(10) NOT NULL
+);
