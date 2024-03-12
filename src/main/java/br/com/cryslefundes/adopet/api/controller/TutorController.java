b4ba3aed70d1d16b62013c2f9a5f8e427dbd4711
Crystian Lefundes <crystian.lf@gmail.com>
2024-03-12 13:31:09 -0300

    feat: Add tutor controller

---
+++ src/main/java/br/com/cryslefundes/adopet/api/controller/TutorController.java
@@ -0,0 +1,46 @@
+package br.com.cryslefundes.adopet.api.controller;
+
+import br.com.cryslefundes.adopet.api.core.dto.tutors.RegisterTutorDTO;
+import br.com.cryslefundes.adopet.api.core.dto.tutors.TutorDTO;
+import br.com.cryslefundes.adopet.api.core.dto.tutors.UpdateTutorDTO;
+import br.com.cryslefundes.adopet.api.service.TutorService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/tutors")
+public class TutorController {
+    @Autowired
+    private TutorService service;
+
+    @GetMapping
+    public ResponseEntity<Page<TutorDTO>> showAllTutors(@PageableDefault(sort = "name") Pageable pagination) {
+        return ResponseEntity.ok(service.showAllTutors(pagination));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TutorDTO> showTutor(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.showTutor(id));
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<TutorDTO> registerTutor(@RequestBody @Valid RegisterTutorDTO dto) {
+        TutorDTO tutorDTO = service.registerTutor(dto);
+        return ResponseEntity.created(tutorDTO.location()).body(tutorDTO);
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<TutorDTO> updateTutor(UpdateTutorDTO dto) {
+        return ResponseEntity.ok(service.updateTutor(dto));
+    }
+}
