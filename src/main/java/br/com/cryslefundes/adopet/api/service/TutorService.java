6d75ba08fd45cce638cb009a2758089065c89201
Crystian Lefundes <crystian.lf@gmail.com>
2024-03-12 13:30:15 -0300

    feat: Add tutor service

---
+++ src/main/java/br/com/cryslefundes/adopet/api/service/TutorService.java
@@ -0,0 +1,51 @@
+package br.com.cryslefundes.adopet.api.service;
+
+import br.com.cryslefundes.adopet.api.core.dto.tutors.RegisterTutorDTO;
+import br.com.cryslefundes.adopet.api.core.dto.tutors.TutorDTO;
+import br.com.cryslefundes.adopet.api.core.dto.tutors.UpdateTutorDTO;
+import br.com.cryslefundes.adopet.api.core.entity.Tutor;
+import br.com.cryslefundes.adopet.api.core.useCase.TutorUseCase;
+import br.com.cryslefundes.adopet.api.repository.TutorRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class TutorService implements TutorUseCase {
+    @Autowired
+    private TutorRepository repository;
+
+    @Override
+    public Page<TutorDTO> showAllTutors(Pageable pagination) {
+        return repository.findAll(pagination).map(TutorDTO::new);
+    }
+
+    @Override
+    public TutorDTO showTutor(UUID id) {
+        Tutor tutor = repository.getReferenceById(id);
+        return new TutorDTO(tutor);
+    }
+
+    @Override
+    public TutorDTO registerTutor(RegisterTutorDTO dto) {
+        Tutor tutor = new Tutor(dto);
+        repository.save(tutor);
+        return new TutorDTO(tutor);
+    }
+
+    @Override
+    public TutorDTO updateTutor(UpdateTutorDTO dto) {
+        Optional<Tutor> mayberTutor = repository.findById(dto.id());
+        if (mayberTutor.isPresent()) {
+            Tutor tutor = mayberTutor.get();
+            tutor.updateInfo(dto);
+            return new TutorDTO(tutor);
+        }
+        throw new EntityNotFoundException();
+    }
+}
