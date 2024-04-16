093d75293ed28411ecde3af29e4003095acff383
Crystian Lefundes <crystian.lf@gmail.com>
2024-04-16 03:14:52 -0300

    refactor: Remove unused import of adoption status

--- src/main/java/br/com/cryslefundes/adopet/api/core/dto/adoption/ApprovedAdoptionDTO.java
+++ src/main/java/br/com/cryslefundes/adopet/api/core/dto/adoption/ApprovedAdoptionDTO.java
@@ -1,6 +1,5 @@
 package br.com.cryslefundes.adopet.api.core.dto.adoption;
 
-import br.com.cryslefundes.adopet.api.core.enums.AdoptionStatus;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
