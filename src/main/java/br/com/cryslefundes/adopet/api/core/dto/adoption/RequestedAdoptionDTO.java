72406de28d0b2f08a53d066882cc7d85d83b46fe
Crystian Lefundes <crystian.lf@gmail.com>
2024-04-16 03:13:44 -0300

    refactor: Change name of tutorId and petId and add a json alias

--- src/main/java/br/com/cryslefundes/adopet/api/core/dto/adoption/RequestedAdoptionDTO.java
+++ src/main/java/br/com/cryslefundes/adopet/api/core/dto/adoption/RequestedAdoptionDTO.java
@@ -1,14 +1,18 @@
 package br.com.cryslefundes.adopet.api.core.dto.adoption;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import java.net.URI;
 import java.util.UUID;
 
 public record RequestedAdoptionDTO(
-        @NotNull UUID idTutor,
-        @NotNull UUID idPet,
+        @NotNull
+        @JsonAlias("tutor_id")
+        UUID tutorId,
+        @NotNull
+        @JsonAlias("pet_id")
+        UUID petId,
         @NotBlank String purpose
 ) {
 }
