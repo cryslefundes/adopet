59977a7225a60060986cc81321e4ac0d324dbea2
Crystian Lefundes <crystian.lf@gmail.com>
2024-05-01 14:30:39 -0300

    feat: Add constructor that accept string with the name of animal type

--- src/main/java/br/com/cryslefundes/adopet/api/core/enums/PetType.java
+++ src/main/java/br/com/cryslefundes/adopet/api/core/enums/PetType.java
@@ -1,12 +1,18 @@
 package br.com.cryslefundes.adopet.api.core.enums;
 
 public enum PetType {
-    DOG,
-    CAT;
+    DOG("dog"),
+    CAT("cat");
+
+    private final String animal;
+
+    PetType(String animal) {
+        this.animal = animal;
+    }
 
     public static PetType fromString(String text) {
         for (PetType type: PetType.values()) {
-            if (type.toString().equalsIgnoreCase(text)) {
+            if (type.animal.equalsIgnoreCase(text)) {
                 return type;
             }
         }
