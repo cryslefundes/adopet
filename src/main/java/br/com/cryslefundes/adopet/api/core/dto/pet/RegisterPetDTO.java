979c39c144067c24f89dcdee5971fb1039c57dd5
Crystian Lefundes <crystian.lf@gmail.com>
2024-05-01 14:21:44 -0300

    refactor: Change pet type name variable and add constraints to age and weight

--- src/main/java/br/com/cryslefundes/adopet/api/core/dto/pet/RegisterPetDTO.java
+++ src/main/java/br/com/cryslefundes/adopet/api/core/dto/pet/RegisterPetDTO.java
@@ -1,17 +1,27 @@
 package br.com.cryslefundes.adopet.api.core.dto.pet;
 
-import br.com.cryslefundes.adopet.api.core.enums.PetType;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.util.UUID;
 
 public record RegisterPetDTO (
         @NotBlank
         String name,
         @NotBlank
         String breed,
+        @PositiveOrZero
+        @Max(value = 20)
         Integer age,
+        @PositiveOrZero
         Float weight,
         @NotNull
-        PetType type
+        @JsonAlias("shelter_id")
+        UUID shelterId,
+        @NotNull
+        String type
 ) {
 }
