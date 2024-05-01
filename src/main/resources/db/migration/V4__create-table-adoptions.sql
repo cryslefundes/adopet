812a1aa1b6d77d80eca0db1d7ed2ac03ab624414
Crystian Lefundes <crystian.lf@gmail.com>
2024-05-01 15:24:20 -0300

    refactor: Change status column type to varchar and removing adoption_status type

--- src/main/resources/db/migration/V4__create-table-adoptions.sql
+++ src/main/resources/db/migration/V4__create-table-adoptions.sql
@@ -1,11 +1,9 @@
-CREATE TYPE ADOPTION_STATUS AS ENUM('DENIED', 'APPROVED', 'WAITING_EVALUATION');
-
 CREATE TABLE adoptions (
     id UUID NOT NULL PRIMARY KEY,
     date_of DATE NOT NULL,
     purpose TEXT,
     justification TEXT,
-    status ADOPTION_STATUS NOT NULL,
+    status VARCHAR(30) NOT NULL,
     tutor_id UUID NOT NULL REFERENCES tutors(id),
     pet_id UUID NOT NULL REFERENCES pets(id)
 );
