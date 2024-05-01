ab4e2099c64bfa077efb5610fbdb70bd215ccbc5
Crystian Lefundes <crystian.lf@gmail.com>
2024-05-01 15:22:51 -0300

    refactor: Change type of age to integer and pet_type to varchar, removing type animal

--- src/main/resources/db/migration/V3__create-table-pets.sql
+++ src/main/resources/db/migration/V3__create-table-pets.sql
@@ -1,11 +1,10 @@
-CREATE TYPE ANIMAL AS ENUM('dog', 'cat');
-
 CREATE TABLE pets (
     id UUID NOT NULL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     breed VARCHAR(100) NOT NULL,
-    age DECIMAL(4,2) NOT NULL,
+    age INTEGER,
     adopted BOOLEAN NOT NULL,
-    pet_type ANIMAL NOT NULL,
+    weight DECIMAL(4,2),
+    pet_type VARCHAR(20) NOT NULL,
     shelter_id UUID NOT NULL REFERENCES shelters(id)
 );
